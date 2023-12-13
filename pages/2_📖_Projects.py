07064c7e951847146631ee21308f467cf51ca424
Sh3ldone <150495179+Sh3ldone@users.noreply.github.com>
2023-12-14 01:12:35 +0800

    Update 2_📖_Projects.py

--- pages/2_📖_Projects.py
+++ pages/2_📖_Projects.py
@@ -21,6 +21,6 @@ with st.container():
         st.subheader("I'm a musician, that's why I created this project so that I can easily convert our band's performance into music MP3.")
 
 st.write("If you wish to convert video into mp3 just click here👇")
-st.write("[convert here ](https://6a3detmwvyutciekoywziu.streamlit.app/)")
+st.write("[convert here ](https://video-mp3-h56r7dxprpuk4ff6sssmtv.streamlit.app/)")
 
 st.subheader("Thank you for visiting my page😉")
